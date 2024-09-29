7d50d303ac38ab91e4d8a3a0b2ffb941741180a0
Josu <josepadillarivas251004@gmail.com>
2024-09-29 14:45:21 -0600

    Commit 1 Proyecto grupo 1

---
+++ ProyectoPJumbo/ProyectoPJumbo/ProyectoPJumbo/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoPJumbo.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public IActionResult Registrarse()
+        {
+            return View();
+        }
+
+        public IActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoPJumbo/ProyectoPJumbo/ProyectoPJumbo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoPJumbo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
