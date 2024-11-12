96dbd63495673604a7587634115d31bc0a4f1e0a
Karen J Román <84334642+UpsKaren@users.noreply.github.com>
2024-11-12 09:11:49 -0600

    Creación del Api y ResgitrarEpleado
    Ya se encuentran los paquetes de Dapper y SQL al igual que el modelo de empleado y respuesta así como el controller de empleado con su único modelo por ahora de registrar empleado

---
+++ PJumboAPI/PJumboAPI/Controllers/EmpleadoController.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using PJumboAPI.Models;
+
+namespace PJumboAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        private readonly IConfiguration _conf;
+
+        public EmpleadoController(IConfiguration conf)
+        {
+            _conf = conf;
+        }
+
+        [HttpPost]
+        [Route("RegistrarEmpleado")]
+        public IActionResult RegistrarEmpleado(Empleado model)
+        {
+
+            using (var context = new SqlConnection(_conf.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var respuesta = new Respuesta();
+                var result = context.Execute("RegistrarEmpleado", new { model.Identificacion, model.Nombre, model.Contrasena,model.CorreoEmpleado,model.Salario,model.IdPuesto });
+
+                if (result > 0)
+                {
+                    respuesta.Codigo = 0;
+                }
+
+                else
+                {
+                    respuesta.Codigo = -1;
+                    respuesta.Mensaje = "Su información no se ha registrado correctamente";
+                }
+
+                return Ok(respuesta);
+            }
+
+        }
+
+    }
+    }
+
---
+++ ProyectoPJumbo/ProyectoPJumbo/ProyectoPJumbo/Models/Respuesta.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoPJumbo.Models
+{
+    public class Respuesta
+    {
+
+
+        public int Codigo { get; set; }
+        public string Mensaje { get; set; } = string.Empty;
+        public object? Contenido { get; set; }
+    }
+}
