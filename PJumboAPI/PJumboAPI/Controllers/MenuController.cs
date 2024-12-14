4c97a2d353ddd70d042270a1b19131701894c1b0
Josueh7 <josueh070499@gmail.com>
2024-12-14 12:20:27 -0600

    CrudPlatosTerminado

--- PJumboAPI/PJumboAPI/Controllers/MenuController.cs
+++ PJumboAPI/PJumboAPI/Controllers/MenuController.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
@@ -16,6 +17,7 @@ namespace PJumboAPI.Controllers
         {
             _conf = conf;
         }
+
 
         [HttpPost]
         [Route("CrearPlato")]
@@ -60,6 +62,77 @@ namespace PJumboAPI.Controllers
                 {
                     respuesta.Codigo = -1;
                     respuesta.Mensaje = "No hay platos registrados en el sistema";
+                }
+
+                return Ok(respuesta);
+            }
+        }
+
+       
+        [HttpGet]
+        [Route("ConsultarPlato")]
+        public IActionResult ConsultarPlato(int ConsecutivoPlato)
+        {
+            using (var context = new SqlConnection(_conf.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var respuesta = new Respuesta();
+                var result = context.QueryFirstOrDefault<Plato>("ConsultarPlato", new { ConsecutivoPlato });
+
+                if (result != null)
+                {
+                    respuesta.Codigo = 0;
+                    respuesta.Contenido = result;
+                }
+                else
+                {
+                    respuesta.Codigo = -1;
+                    respuesta.Mensaje = "No se encontró la información del Plato";
+                }
+
+                return Ok(respuesta);
+            }
+        }
+
+        [HttpPut]
+        [Route("ActualizarPlato")]
+        public IActionResult ActualizarPlato(Plato plato)
+        {
+            using (var context = new SqlConnection(_conf.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var respuesta = new Respuesta();
+                var result = context.Execute("ActualizarPlato", new { plato.ConsecutivoPlato, plato.Nombre, plato.Precio, plato.Descripcion});
+
+                if (result > 0)
+                {
+                    respuesta.Codigo = 0;
+                }
+                else
+                {
+                    respuesta.Codigo = -1;
+                    respuesta.Mensaje = "La información del plato no se ha actualizado correctamente";
+                }
+
+                return Ok(respuesta);
+            }
+        }
+
+        [HttpPut]
+        [Route("ActualizarEstado")]
+        public IActionResult ActualizarEstado(Plato plato)
+        {
+            using (var context = new SqlConnection(_conf.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var respuesta = new Respuesta();
+                var result = context.Execute("ActualizarEstadoPlato", new { plato.ConsecutivoPlato });
+
+                if (result > 0)
+                {
+                    respuesta.Codigo = 0;
+                }
+                else
+                {
+                    respuesta.Codigo = -1;
+                    respuesta.Mensaje = "El estado del plato no se ha actualizado correctamente";
                 }
 
                 return Ok(respuesta);
